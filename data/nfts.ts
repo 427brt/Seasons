9dc6c1e148a420d2e937cd3afdffec9f52128fa5
427brt <yu0427suzuki@icloud.com>
2023-05-18 14:20:34 +0900

    first

---
+++ data/nfts.ts
@@ -0,0 +1,40 @@
+import { NFT } from "../types/NFT";
+
+export const nfts: NFT[] = [
+  {
+    id: 0,
+    name: "Spring",
+    description: "I like Spring.",
+    url: "https://ipfs.thirdwebcdn.com/ipfs/QmXNJ56fYpSxZPML4hfy5xVLaTZ4NmujNUUasmMy3EsATt/%E3%83%86%E3%82%AD%E3%82%B9%E3%83%88%E3%81%AE%E7%94%BB%E5%83%8F%E5%8C%96%20(1).png",
+    price: 0.0,
+    attributeNameOne: "season",
+    valueNameOne: "spring",
+  },
+  {
+    id: 1,
+    name: "Summer",
+    description: "I like Summer.",
+    url: "https://ipfs.thirdwebcdn.com/ipfs/QmZDiavxRmUWJMBaAYJMiYFwfaSzoBfPpFjUd3MS8fRJwA/%E3%83%86%E3%82%AD%E3%82%B9%E3%83%88%E3%81%AE%E7%94%BB%E5%83%8F%E5%8C%96%20(2).png",
+    price: 0.0,
+    attributeNameOne: "season",
+    valueNameOne: "summer",
+  },
+  {
+    id: 2,
+    name: "Autumn",
+    description: "I like Autumn.",
+    url: "https://ipfs.thirdwebcdn.com/ipfs/QmNMoQYMrhz7ajWVkRzPfLxT81wLAonE6SApHuGHHwuEHs/%E3%83%86%E3%82%AD%E3%82%B9%E3%83%88%E3%81%AE%E7%94%BB%E5%83%8F%E5%8C%96%20(3).png",
+    price: 0.0,
+    attributeNameOne: "season",
+    valueNameOne: "autumn",
+  },
+  {
+    id: 3,
+    name: "Winter",
+    description: "I like Winter.",
+    url: "https://ipfs.thirdwebcdn.com/ipfs/QmWGJwJ8kQVXAkYTzpR39pUx8NRthXLczjkhpSkroRwX7H/%E3%83%86%E3%82%AD%E3%82%B9%E3%83%88%E3%81%AE%E7%94%BB%E5%83%8F%E5%8C%96%20(4).png",
+    price: 0.0,
+    attributeNameOne: "season",
+    valueNameOne: "winter",
+  },
+];
---
+++ types/NFT.ts
@@ -0,0 +1,10 @@
+export interface NFT {
+  id: number;
+  name: string;
+  description: string;
+  url: string;
+  price: number;
+  minted?: boolean;
+  attributeNameOne: string;
+  valueNameOne: string
+}
